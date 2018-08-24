bbe865efbd2216326669918baea7b6a5ea03f60c
juanf <juanf@DESKTOP-C5LQI72>
2018-08-23 22:09:09 -0500

    Option added

--- Laboratorio1/src/controller/MainViewController.java
+++ Laboratorio1/src/controller/MainViewController.java
@@ -1,198 +1,218 @@
 package controller;
 
 import java.net.URL;
+import java.util.Arrays;
 import java.util.ResourceBundle;
+
+import javax.swing.JOptionPane;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
 import javafx.scene.control.Tab;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import model.Coprocessor;
 import javafx.fxml.Initializable;
 
-public class MainViewController implements Initializable{
+public class MainViewController implements Initializable {
+	public static final String[] CB_ELEMENTS = { "Generar valores ordenados", "Generar valores ordenados inversamente",
+			"Generar valores en orden aleatorio", "Generar valores en un porcentaje" };
 
-	 @FXML
-	    private Tab tabUser;
+	@FXML
+	private Tab tabUser;
 
-	    @FXML
-	    private TextField txtValues;
+	@FXML
+	private TextField txtValues;
 
-	    @FXML
-	    private Button btnSort;
+	@FXML
+	private Button btnSort;
 
-	    @FXML
-	    private Tab tabRamdon;
+	@FXML
+	private Tab tabRamdon;
 
-	    @FXML
-	    private TextField txtAmount;
+	@FXML
+	private TextField txtAmount;
 
-	    @FXML
-	    private TextField txtStart;
+	@FXML
+	private TextField txtStart;
 
-	    @FXML
-	    private TextField txtEnd;
+	@FXML
+	private TextField txtEnd;
 
-	    @FXML
-	    private ComboBox<String> cbTypes;
+	@FXML
+	private ComboBox<String> cbTypes;
 
-	    @FXML
-	    private Button btnGenerate;
-	    
-	    @FXML
-	    private TextArea areaNumbers;
-	    
-	    @FXML
-	    private Button btnOrderRandom;
-	    
-	    @FXML
-	    private Button btnAgregar;
-	    
-	    @FXML
-	    private TextArea areaValues;
-	    
-	    /**
-	     * Relations--------------
-	     */
-	    private Coprocessor processor;
-	    
-	    
+	@FXML
+	private Button btnGenerate;
+
+	@FXML
+	private TextArea areaNumbers;
+
+	@FXML
+	private Button btnOrderRandom;
+
+	@FXML
+	private Button btnAgregar;
+
+	@FXML
+	private TextArea areaValues;
 	
+    @FXML
+    private Label lblPercent;
+
+	/**
+	 * Relations--------------
+	 */
+	private Coprocessor processor;
+
+	
+	private int option;
 	public MainViewController() {
-			
-			processor = new Coprocessor();
-		}
+		
+		option = 0;
 
-
+		processor = new Coprocessor();
+	}
 
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
 		// TODO Auto-generated method stub
+		lblPercent.setVisible(false);
 		cbTypes.getItems().add("Generar valores ordenados");
 		cbTypes.getItems().add("Generar valores ordenados inversamente");
 		cbTypes.getItems().add("Generar valores en orden aleatorio");
 		cbTypes.getItems().add("Generar valores en un porcentaje");
-		
-		
+
 	}
-
-
 
 	public String getTxtValues() {
 		return txtValues.getText();
 	}
 
-
-
 	public void setTxtValues(String values) {
 		txtValues.setText(values);
 	}
-
-
 
 	public String getTxtAmount() {
 		return txtAmount.getText();
 	}
 
-
-
 	public void setTxtAmount(TextField txtAmount) {
 		this.txtAmount = txtAmount;
 	}
-
-
 
 	public String getTxtStart() {
 		return txtStart.getText();
 	}
 
-
-
 	public void setTxtStart(TextField txtStart) {
 		this.txtStart = txtStart;
 	}
-
-
 
 	public String getTxtEnd() {
 		return txtEnd.getText();
 	}
 
-
-
 	public void setTxtEnd(TextField txtEnd) {
 		this.txtEnd = txtEnd;
 	}
 
-
-
-	public ComboBox<String> getCbTypes() {
-		return cbTypes;
+	public String getCbTypes() {
+		return cbTypes.getValue();
 	}
-
-
 
 	public void setCbTypes(ComboBox<String> cbTypes) {
 		this.cbTypes = cbTypes;
 	}
-	
-	
-	
+
 	public void setAreaNumbers(String numbers) {
 		areaNumbers.setText(numbers);
 	}
-
-
 
 	public TextArea getAreaValues() {
 		return areaValues;
 	}
 
-
-
 	public void setAreaValues(String values) {
-		areaValues.setText(values);;
+		areaValues.setText(values);
+		;
 	}
 
-
-
 	@FXML
-    void generateNumbers(ActionEvent event) {
-		System.out.println("click");
-		int amount = Integer.parseInt(getTxtAmount());
-		int min = Integer.parseInt(getTxtStart());
-		int max = Integer.parseInt(getTxtEnd());
-		processor.generateRandom(amount, min, max);
-		setAreaNumbers(processor.toString());
+	void generateNumbers(ActionEvent event) {
+		lblPercent.setVisible(true);
+
+		try {
+			int amount = Integer.parseInt(getTxtAmount());
+			int min = Integer.parseInt(getTxtStart());
+			int max = Integer.parseInt(getTxtEnd());			
+			if (getCbTypes().equals(CB_ELEMENTS[0])) {
+				System.out.println("opcion 1");
+				option = 1;
+				
+				processor.generateRandom(amount, min, max);
+				int[] arr = processor.countingSort(processor.getArrNumInt());
+				setAreaNumbers(Arrays.toString(arr));
+//				setAreaNumbers(processor.toString());
+			} else if (getCbTypes().equals(CB_ELEMENTS[1])) {
+				option = 2;
+			} else if (getCbTypes().equals(CB_ELEMENTS[2])) {
+				option = 3;
+				processor.generateRandom(amount, min, max);
+				setAreaNumbers(processor.toString());
+			} else {
+				option = 4;
+			}
+			
+			
+		} catch (Exception e) {
+			JOptionPane.showMessageDialog(null, "Debe escribir numeros en todos los campos");
+		}
 		
-    }
-	
+
+	}
+
+	// Method to sort numbers generated randomly
 	@FXML
-    void makeSort(ActionEvent event) {
-		int amount = Integer.parseInt(getTxtAmount());
-		int maxValue = Integer.parseInt(getTxtEnd());
-//		processor.changeSize(amount);
-		processor.bucketSort(maxValue);
-		setAreaNumbers(processor.toString());
-    }
-	
+	void makeSort(ActionEvent event) {
+
+		if (processor.getArrNumInt().length == 0) {
+			JOptionPane.showMessageDialog(null, "Genere los números a ordenar primero");
+		} else {
+			int[] arr = processor.countingSort(processor.getArrNumInt());
+			setAreaNumbers(Arrays.toString(arr));
+		}
+//		if (getCbTypes() != null) {
+//			
+//		} else {
+//			JOptionPane.showMessageDialog(null, "Seleccion una forma de ordenar");
+//
+//		}
+		
+
+//			int amount = Integer.parseInt(getTxtAmount());
+//			int maxValue = Integer.parseInt(getTxtEnd());
+//			processor.bucketSort(maxValue);
+//			setAreaNumbers(processor.toString());
+
+	}
+
+	// Method to sort values inserted by the user
 	@FXML
-    void sortNumbers(ActionEvent event) {
+	void sortNumbers(ActionEvent event) {
 		processor.sortArrayList();
 		setAreaValues(processor.getNumbersInt().toString());
 
-    }
-	
+	}
 
-    @FXML
-    void addValues(ActionEvent event) {
-    	processor.addNumbersInt(Integer.parseInt(getTxtValues()));
-    	setTxtValues("");
-    	setAreaValues(processor.getNumbersInt().toString());
-    }
-	
+	@FXML
+	void addValues(ActionEvent event) {
+		processor.addNumbersInt(Integer.parseInt(getTxtValues()));
+		setTxtValues("");
+		setAreaValues(processor.getNumbersInt().toString());
+	}
 
 }
--- Laboratorio1/src/model/Coprocessor.java
+++ Laboratorio1/src/model/Coprocessor.java
@@ -39,7 +39,7 @@ public class Coprocessor {
 		
 	}
 	
-	
+	//Use this better for floats numbers
 	public void bucketSort(int maxValue) {
 		int bucket [] = new int[maxValue+1];
 		for (int i = 0; i < bucket.length; i++) {
@@ -60,10 +60,117 @@ public class Coprocessor {
 	public void sortArrayList() {
 		Collections.sort(numbersInt);
 	}
-
-	public void countingSort() {
-		ArrayList<Integer> test = new ArrayList<>(numbersInt);
-		System.out.println(test.toString());
+	
+	//Use this just for Integer numbers
+	public int[] countingSort(int [] array ) {
+//		int [] aux = new int [arrNumInt.length];
+//		int min = arrNumInt[0];
+//		int max = arrNumInt[0];
+//		for (int i = 0; i < arrNumInt.length; i++) {
+//			if (arrNumInt[i] < min) {
+//				min = arrNumInt[i];
+//			}
+//			else if (arrNumInt[i] > max) {
+//				max = arrNumInt[i];
+//			}
+//		}
+//		
+//		int[] counts = new int[max-min+1];
+//		for (int i = 0; i < arrNumInt.length; i++) {
+//			counts[arrNumInt[i] - min]++;			
+//		}
+//		counts[0]--;
+//		for (int i = 1; i < counts.length; i++) {
+//			counts[i] = counts[i] + counts[i-1];
+//		}
+//		for (int i = arrNumInt.length - 1; i >= 0; i--) {
+//			arrNumInt[counts[aux[i] - min]--] = aux[i];
+//	    }
+		 // array to be sorted in, this array is necessary
+	    // when we sort object datatypes, if we don't, 
+	    // we can sort directly into the input array     
+	    int[] aux = new int[array.length];
+	 
+	    // find the smallest and the largest value
+	    int min = array[0];
+	    int max = array[0];
+	    for (int i = 1; i < array.length; i++) {
+	      if (array[i] < min) {
+	        min = array[i];
+	      } else if (array[i] > max) {
+	        max = array[i];
+	      }
+	    }
+	 
+	    // init array of frequencies
+	    int[] counts = new int[max - min + 1];
+	 
+	    // init the frequencies
+	    for (int i = 0;  i < array.length; i++) {
+	      counts[array[i] - min]++;
+	    }
+	 
+	    // recalculate the array - create the array of occurences
+	    counts[0]--;
+	    for (int i = 1; i < counts.length; i++) {
+	      counts[i] = counts[i] + counts[i-1];
+	    }
+	 
+	    /*
+	      Sort the array right to the left
+	      1) Look up in the array of occurences the last occurence of the given value
+	      2) Place it into the sorted array
+	      3) Decrement the index of the last occurence of the given value
+	      4) Continue with the previous value of the input array (goto set1), 
+	         terminate if all values were already sorted
+	    */ 
+	    for (int i = array.length - 1; i >= 0; i--) {
+	        aux[counts[array[i] - min]--] = array[i];
+	    }
+	 
+	    return aux;
+	}
+	public int[] countingSortInverse (int [] array ) { 
+		int[] aux = new int[array.length];
+		 
+	    // find the smallest and the largest value
+	    int min = array[0];
+	    int max = array[0];
+	    for (int i = 1; i < array.length; i++) {
+	      if (array[i] < min) {
+	        min = array[i];
+	      } else if (array[i] > max) {
+	        max = array[i];
+	      }
+	    }
+	 
+	    // init array of frequencies
+	    int[] counts = new int[max - min + 1];
+	 
+	    // init the frequencies
+	    for (int i = 0;  i < array.length; i++) {
+	      counts[array[i] - min]++;
+	    }
+	 
+	    // recalculate the array - create the array of occurences
+	    counts[0]--;
+	    for (int i = 1; i < counts.length; i++) {
+	      counts[i] = counts[i] + counts[i-1];
+	    }
+	 
+	    /*
+	      Sort the array right to the left
+	      1) Look up in the array of occurences the last occurence of the given value
+	      2) Place it into the sorted array
+	      3) Decrement the index of the last occurence of the given value
+	      4) Continue with the previous value of the input array (goto set1), 
+	         terminate if all values were already sorted
+	    */ 
+	    for (int i = array.length - 1; i >= 0; i--) {
+	        aux[counts[array[i] - min]--] = array[i];
+	    }
+	 
+	    return aux;
 	}
 
 	
@@ -87,7 +194,14 @@ public class Coprocessor {
 	public void addNumbersInt(int number) {
 		numbersInt.add(number);
 	}
+	
 
+	public int[] getArrNumInt() {
+		return arrNumInt;
+	}
+	public void setArrNumInt(int[] arrNumInt) {
+		this.arrNumInt = arrNumInt;
+	}
 	@Override
 	public String toString() {
 		// TODO Auto-generated method stub
